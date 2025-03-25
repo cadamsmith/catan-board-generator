48ac44a5f109421f12379ad51b99f2462b4c6bde
cadamsmith <cadamsmith.dev@gmail.com>
2025-03-24 21:51:31 -0500

    Implement hexagon grid drawing functionality for Catan board generator

--- src/main.js
+++ src/main.js
@@ -137,26 +137,77 @@ const MATH_CONSTANTS = {
   SIXTY_DEGREES: Math.PI / 3,
 };
 
+const settings = {
+  hexes: [],
+};
+
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+ctx.textAlign = "center";
+
+const r = 50;
+
+drawGrid(5, 5);
+
+/**
+ * Draws a grid of hexagons of given width and height
+ * @param {number} width - How many hexagons wide the grid is
+ * @param {number} height - How many hexagons tall the grid is
+ */
+function drawGrid(width, height) {
+  const topLeft = [100, 100];
+
+  const center = [...topLeft];
+
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++) {
+      const points = drawHexagon(center[0], center[1]);
+
+      const hex = {
+        center,
+        points,
+        coordinates: [i, j],
+        type: 'hex',
+      };
+
+      settings.hexes.push(hex);
+
+      center[0] += 2 * r * Math.sin(MATH_CONSTANTS.SIXTY_DEGREES);
+    }
+
+    if (j % 2 === 0) {
+      center[0] = topLeft[0] + r * Math.sin(MATH_CONSTANTS.SIXTY_DEGREES);
+    } else {
+      center[0] = topLeft[0];
+    }
+    center[1] += (3 * r) / 2;
+  }
+}
+
 /**
  * Draws a hexagon at the given coordinates with the given radius
  * @param {number} x - The x coordinate of the center of the hexagon
  * @param {number} y - The y coordinate of the center of the hexagon
  * @param {number} r - The radius of the hexagon
+ * @returns {number[][]} The coordinates of the 6 hexagon points
  */
-function drawHexagon(x, y, r) {
+function drawHexagon(x, y) {
+  const points = [];
+
   ctx.beginPath();
   for (let i = 0; i < 6; i++) {
     const angle = MATH_CONSTANTS.SIXTY_DEGREES * i;
 
-    const point = [
-      x + r * Math.sin(angle),
-      y + r * Math.cos(angle),
-    ];
+    const point = [x + r * Math.sin(angle), y + r * Math.cos(angle)];
+
+    points.push(point);
 
     ctx.lineTo(point[0], point[1]);
   }
   ctx.closePath();
   ctx.stroke();
+
+  return points;
 }
 
 /**
