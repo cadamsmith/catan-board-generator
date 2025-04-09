a1f2827236e90fffacb7cad9a20c84d7c04fb268
cadamsmith <cadamsmith.dev@gmail.com>
2025-04-09 15:55:37 -0500

    changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const GAME_TYPES = {
   BASE_3_4: enumValue("BASE_3_4"),
+  BASE_5_6: enumValue("BASE_5_6"),
 };
 
 const RESOURCES = {
@@ -41,16 +42,62 @@ const settings = {
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
-ctx.font = "24px Arial";
+ctx.font = "16px Arial";
 ctx.textAlign = "center";
 ctx.textBaseline = "middle";
 
 const padding = 30;
 
-const config = getBoardConfig(GAME_TYPES.BASE_3_4);
+let config;
 
-init(config);
+document.querySelector("#generate-board-btn").addEventListener("click", () => {
+  if (document.querySelector("#base34").checked) {
+    config = getBoardConfig(GAME_TYPES.BASE_3_4);
+  } else {
+    config = getBoardConfig(GAME_TYPES.BASE_5_6);
+  }
 
+  init(config);
+
+  displayResourceCardCounts(config);
+  displayDevCardCounts(config);
+
+  document.querySelector(".wizard-steps").classList.add("page-2");
+});
+
+document.querySelector(".prev-step-btn").addEventListener("click", () => {
+  document.querySelector(".wizard-steps").classList.remove("page-2");
+});
+
+function displayResourceCardCounts(config) {
+  const resourceCardCounts = config.resourceCardCounts;
+  const wrapperElement = document.getElementById("resource-card-counts");
+
+  for (let i = 0; i < resourceCardCounts.length; i++) {
+    const resourceCardCount = resourceCardCounts[i];
+
+    const resourceCardCountElement = wrapperElement.querySelector(
+      `[data-id="${resourceCardCount.resource}"]`
+    );
+
+    resourceCardCountElement.textContent = `${resourceCardCount.resource}: ${resourceCardCount.count}`;
+  }
+}
+
+function displayDevCardCounts(config) {
+  const devCardCounts = config.devCardCounts;
+  const wrapperElement = document.getElementById("dev-card-counts");
+
+  for (let i = 0; i < devCardCounts.length; i++) {
+    const devCardCount = devCardCounts[i];
+
+    const devCardCountElement = wrapperElement.querySelector(
+      `[data-id="${devCardCount.type}"]`
+    );
+
+    devCardCountElement.textContent = `${devCardCount.type}: ${devCardCount.count}`;
+  }
+}
 /**
  * Initializes the board
  * @param {Object} config - The configuration object
@@ -70,6 +117,8 @@ function getBoardConfig(gameType) {
   switch (gameType) {
     case GAME_TYPES.BASE_3_4:
       return getBase34Config();
+    case GAME_TYPES.BASE_5_6:
+      return getBase56Config();
     default:
       throw new Error(`Invalid game type: ${gameType}`);
   }
@@ -202,6 +251,139 @@ function getBase34Config() {
 }
 
 /**
+ * Returns the configuration object for the base 5-6 player game type
+ * @returns {Object} The configuration object
+ */
+function getBase56Config() {
+  const harborCounts = [
+    {
+      resource: RESOURCES.ANY,
+      cost: 3,
+      count: 4,
+    },
+    {
+      resource: RESOURCES.LUMBER,
+      cost: 2,
+    },
+    {
+      resource: RESOURCES.BRICK,
+      cost: 2,
+    },
+    {
+      resource: RESOURCES.WOOL,
+      cost: 1,
+    },
+    {
+      resource: RESOURCES.GRAIN,
+      cost: 1,
+    },
+    {
+      resource: RESOURCES.ORE,
+      cost: 1,
+    },
+  ];
+
+  const resourceCardCounts = [
+    {
+      resource: RESOURCES.BRICK,
+      count: 19,
+    },
+    {
+      resource: RESOURCES.LUMBER,
+      count: 19,
+    },
+    {
+      resource: RESOURCES.WOOL,
+      count: 19,
+    },
+    {
+      resource: RESOURCES.GRAIN,
+      count: 19,
+    },
+    {
+      resource: RESOURCES.ORE,
+      count: 19,
+    },
+  ];
+
+  const hexTileCounts = [
+    {
+      type: HEX_TYPES.HILLS,
+      count: 5,
+    },
+    {
+      type: HEX_TYPES.FOREST,
+      count: 6,
+    },
+    {
+      type: HEX_TYPES.MOUNTAINS,
+      count: 5,
+    },
+    {
+      type: HEX_TYPES.FIELDS,
+      count: 6,
+    },
+    {
+      type: HEX_TYPES.PASTURE,
+      count: 6,
+    },
+    {
+      type: HEX_TYPES.DESERT,
+      count: 2,
+    },
+  ];
+
+  const devCardCounts = [
+    {
+      type: DEV_CARD_TYPES.KNIGHT,
+      count: 14,
+    },
+    {
+      type: DEV_CARD_TYPES.ROAD_BUILDING,
+      count: 2,
+    },
+    {
+      type: DEV_CARD_TYPES.YEAR_OF_PLENTY,
+      count: 2,
+    },
+    {
+      type: DEV_CARD_TYPES.MONOPOLY,
+      count: 2,
+    },
+    {
+      type: DEV_CARD_TYPES.VICTORY_POINT,
+      count: 5,
+    },
+  ];
+
+  const baseMap = [
+    [0, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 0]
+  ];
+
+  const numberTokens = [
+    2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6,
+    8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12
+  ];
+
+  const config = {
+    hexTileCounts,
+    number_tokens: numberTokens,
+    harborCounts,
+    resourceCardCounts,
+    devCardCounts,
+    baseMap,
+  };
+
+  return config;
+}
+
+/**
  * Processes the configuration object, initializing the board state
  * @param {Object} config - The configuration object
  */
@@ -272,6 +454,8 @@ function shuffleHexTiles(hexTileCounts) {
  * @param {number} height - How many hexagons tall the grid is
  */
 function drawGrid(width, height) {
+  document.getElementById("canvas-container").classList.remove("d-none");
+
   // clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -305,15 +489,15 @@ function drawGrid(width, height) {
 
       if (hexData.numberToken != -1) {
         ctx.beginPath();
-        ctx.arc(center[0], center[1], 30, 0, 2 * Math.PI);
+        ctx.arc(center[0], center[1], 20, 0, 2 * Math.PI);
         ctx.stroke();
 
         // fill circle
-        ctx.fillStyle = "#fff";
+        ctx.fillStyle = getNumberTokenColor(hexData.numberToken);
         ctx.fill();
 
         // write coords in the center of the hexagon
-        ctx.fillStyle = getNumberTokenColor(hexData.numberToken);
+        ctx.fillStyle = "#000";
         ctx.fillText(hexData.numberToken, center[0], center[1]);
       }
 
@@ -350,10 +534,10 @@ function getHexFillColor(hexType) {
 
 function getNumberTokenColor(numberToken) {
   if (numberToken === 6 || numberToken === 8) {
-    return "#F00";
+    return "#FFA8B5";
   }
 
-  return "#000";
+  return "#fff";
 }
 
 /**
@@ -440,26 +624,6 @@ function getCanvasWidth() {
  */
 function getCanvasHeight() {
   return canvas.height - 2 * padding;
-}
-
-/**
- * Creates an enum with the given name and values
- * @param {string} name - The name of the enum
- * @param {string[]} values - The values of the enum
- * @returns {Object} The enum
- */
-function makeEnum(name, values) {
-  const enumValue = (name) => Object.freeze({ toString: () => name });
-
-  return Object.freeze(
-    values.reduce(
-      (acc, val) => ({
-        ...acc,
-        [val]: enumValue(`${name}.${val}`),
-      }),
-      {}
-    )
-  );
 }
 
 function shuffle(arr) {
